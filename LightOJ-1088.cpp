0b0d99e0e07bcb0d1041eada2393df335aacffdf
Mia Hossain <miahossain8888@gmail.com>
2020-01-21 12:40:49 +0600

    binary search problem solution

--- LightOJ-1088.cpp
+++ LightOJ-1088.cpp
@@ -4,34 +4,27 @@ typedef long long ll;
 int main()
 {
     int t;
-    //cin >> t;
     scanf("%d", &t);
     for(int k = 1; k <= t; k++)
     {
         int n, q;
-        //cin >> n >> q;
         scanf("%d%d", &n, &q);
-        //int arr[100005] = {};
-        //set<int>ss;
         vector<int>arr;
         for(int i = 1; i <= n; i++)
         {
             int te;
             scanf("%d", &te);
             arr.push_back(te);
-            //ss.insert(te);
         }
         sort(arr.begin(), arr.end());
         cout << "Case " << k << ":" << endl;
         while(q--)
         {
             int a, b;
-            //cin >> a >> b;
             scanf("%d%d", &a, &b);
             bool ok = false;
             int ff = lower_bound(arr.begin(), arr.end(), a)-arr.begin();
             int ss = upper_bound(arr.begin(), arr.end(), b)-arr.begin();
-            //cout << ss-ff << endl;
             printf("%d\n", ss-ff);
         }
     }
